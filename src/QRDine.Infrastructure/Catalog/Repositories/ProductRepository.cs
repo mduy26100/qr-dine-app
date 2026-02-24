31b37075582eabad9fdfdc87a71332b8649401de
MDuy <manhduy261000@gmail.com>
2026-02-24 13:38:35 +0700

    feat(catalog): add ProductRepository abstraction and implementation

---
+++ src/QRDine.Infrastructure/Catalog/Repositories/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using QRDine.Application.Features.Catalog.Repositories;
+using QRDine.Domain.Catalog;
+using QRDine.Infrastructure.Persistence;
+using QRDine.Infrastructure.Persistence.Repositories;
+
+namespace QRDine.Infrastructure.Catalog.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
