155c264fb288bcd0c538a467ec079eb4b4aec54b
MDuy <manhduy261000@gmail.com>
2026-02-26 23:25:00 +0700

    feat(catalog): add fluent validation for storefront products query
    - Prevent empty Guid queries for MerchantId and CategoryId in GetProductsByCategoryQuery.

---
+++ src/QRDine.Application/Features/Catalog/Products/Queries/GetProductsByCategory/GetProductsByCategoryQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace QRDine.Application.Features.Catalog.Products.Queries.GetProductsByCategory
+{
+    public class GetProductsByCategoryQueryValidator : AbstractValidator<GetProductsByCategoryQuery>
+    {
+        public GetProductsByCategoryQueryValidator()
+        {
+            RuleFor(x => x.MerchantId)
+                .NotEmpty()
+                .WithMessage("MerchantId is required for storefront catalog.");
+
+            RuleFor(x => x.CategoryId)
+                .NotEmpty()
+                .WithMessage("CategoryId is required to fetch products.");
+        }
+    }
+}
