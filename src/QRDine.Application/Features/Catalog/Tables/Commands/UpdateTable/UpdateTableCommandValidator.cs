12ab561ebedc4a67e892f9d5e88aafd696685a53
MDuy <manhduy261000@gmail.com>
2026-02-27 15:04:48 +0700

    feat(catalog): add FluentValidation for UpdateTableCommand

--- src/QRDine.Application/Features/Catalog/Tables/Commands/UpdateTable/UpdateTableCommandValidator.cs
+++ src/QRDine.Application/Features/Catalog/Tables/Commands/UpdateTable/UpdateTableCommandValidator.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QRDine.Application.Features.Catalog.Tables.Commands.UpdateTable
+﻿namespace QRDine.Application.Features.Catalog.Tables.Commands.UpdateTable
 {
-    internal class UpdateTableCommandValidator
+    public class UpdateTableCommandValidator : AbstractValidator<UpdateTableCommand>
     {
+        public UpdateTableCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Table ID is required.");
+
+            RuleFor(x => x.Dto)
+                .NotNull().WithMessage("Table data is required.");
+
+            RuleFor(x => x.Dto.Name)
+                .NotEmpty().WithMessage("Table name is required.")
+                .MaximumLength(100).WithMessage("Table name must not exceed 100 characters.");
+        }
     }
 }
