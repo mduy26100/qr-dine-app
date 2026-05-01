6c6bea4df0e480d147d8f612a1d056e14431999a
MDuy <manhduy261000@gmail.com>
2026-05-01 22:42:56 +0700

    feat(jobs): implement generic hangfire background job plugin with delegated storage configuration

---
+++ SharedKernel/Plugins/SharedKernel.BackgroundJobs/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿namespace SharedKernel.BackgroundJobs
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddSharedHangfireBackgroundJobs(this IServiceCollection services, 
+            Action<IServiceProvider, IGlobalConfiguration> configureStorage)
+        {
+            if (configureStorage == null)
+            {
+                throw new ArgumentNullException(nameof(configureStorage), "You need to configure a Storage Provider for Hangfire (e.g., SQL Server, Redis...).");
+            }
+
+            services.AddHangfire((sp, config) =>
+            {
+                config.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                      .UseSimpleAssemblyNameTypeSerializer()
+                      .UseRecommendedSerializerSettings();
+
+                configureStorage.Invoke(sp, config);
+            });
+
+            services.AddHangfireServer();
+
+            services.AddTransient<IBackgroundJobService, HangfireBackgroundJobService>();
+
+            return services;
+        }
+    }
+}
---
+++ SharedKernel/Plugins/SharedKernel.BackgroundJobs/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Hangfire;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Linq.Expressions;
+global using SharedKernel.Application.Interfaces.BackgroundJobs;
+global using SharedKernel.BackgroundJobs.Services;
